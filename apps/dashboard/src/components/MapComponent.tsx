e94345bedd9f67b4ed6261d6d8b41f8d1c4635b2
leaderofARS <abhayrshanbhag@hotmail.com>
2026-05-09 18:40:53 +0530

    style: switch to colorful Voyager map tiles for better visibility

--- apps/dashboard/src/components/MapComponent.tsx
+++ apps/dashboard/src/components/MapComponent.tsx
@@ -44,12 +44,12 @@ export default function MapComponent({ incidents, selected, onSelect }: MapCompo
     <MapContainer
       center={center}
       zoom={12}
-      style={{ width: "100%", height: "100%", background: "#06060f" }}
+      style={{ width: "100%", height: "100%" }}
     >
       <ChangeView center={center} />
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
       />
       {incidents.map((inc) => (
         <Marker
@@ -73,7 +73,7 @@ export default function MapComponent({ incidents, selected, onSelect }: MapCompo
           })}
         >
           {selected?.id === inc.id && (
-            <Popup position={[inc.centroid_lat, inc.centroid_lon]} onClose={() => onSelect(null)}>
+            <Popup eventHandlers={{ remove: () => onSelect(null) }}>
               <div style={{ color: "#111827", minWidth: "160px" }}>
                 <strong style={{ display: "block", marginBottom: "4px" }}>{inc.status} Incident</strong>
                 <div style={{ fontSize: "0.85rem" }}>{inc.witness_count} witnesses reported</div>
